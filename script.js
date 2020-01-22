79a1f84feffc40879e77c36bcfb6ac92e6411f43
mahatodaya <mahatodayaram1@gmail.com>
2020-01-22 21:26:51 +0545

    all web pages are created

---
+++ script.js
@@ -0,0 +1,11 @@
+function loginClicked() {
+    $('#myModal').modal('show')
+}
+
+// var modal = document.getElementById('id01'); 
+  
+//         window.onclick = function(event) { 
+//             if (event.target == modal) { 
+//                 modal.style.display = "none"; 
+//             } 
+//         }
